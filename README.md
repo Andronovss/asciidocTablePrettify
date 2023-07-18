3c92345975f04b53ccdc43ab20d6bda81640bad8
S. Andronov <andronovss@icloud.com>
2023-07-19 01:14:37 +0300

    added the new file: script.py || new content in the README file

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # asciidocTablePrettify
+
 This repository contains a script that visually formats simple tables in asciidoc files into a more readable format.
+
+The script allows you to render tables of the following type:
+
+```adoc
+|===
+|Header 1        |Header 2        |Header 3
+
+|Column 1, row 1                                                |Column 2, row 1 |Column 3, row 1
+
+|Column 1, row 2|Column 2, row 2 |Column 3, row 2
+
+|Column 1, row 3  |Column 2, row 3 |Column 3, row 3
+|=
+```
+
+Into a more visualized view:
+
+```adoc
+|===
+| Header 1        | Header 2        | Header 3       
+| Column 1, row 1 | Column 2, row 1 | Column 3, row 1
+| Column 1, row 2 | Column 2, row 2 | Column 3, row 2
+| Column 1, row 3 | Column 2, row 3 | Column 3, row 3
+|===
+```
+
+As a result, the script will create a file **output.adoc**, which will be a copy of the selected file, but with formatted tables.
+
+> **Note**
+> The contents in **output.adoc** file are always overwritten after the script is run!
+
+## How to use
+
+Just run the script with the following command:
+
+`python script.py`
+
+At that, the console will display help. Or you can specify as an argument the file in which you want to format the tables:
+
+`python script.py input.adoc`
---
+++ script.py
@@ -0,0 +1,88 @@
+import sys
+import os
+import re
+import pypandoc
+
+def align_vertical_bars_in_tables(adoc_content):
+    # Regular expression for Asciidoc table lookup
+    table_pattern = r'\|===([\s\S]*?)\|==='
+
+    def process_table(match):
+        # Get table contents from regular expression matching
+        table = match.group(1)
+        # Split the table into rows
+        lines = table.strip().split('\n')
+
+        # Determining the maximum number of symbols in each cell
+        max_lengths = [0] * len(lines[0].split('|'))
+        new_lines = []
+
+        for line in lines:
+            # If there are no | symbols in the string, we skip it because it is an extra row
+            if '|' not in line:
+                continue
+            cells = line.strip().split('|')
+            for i, cell in enumerate(cells):
+                # Find the maximum length for each cell
+                max_lengths[i] = max(max_lengths[i], len(cell.strip()))
+            # Save only rows with cells, excluding unnecessary rows
+            new_lines.append(cells)
+
+        aligned_lines = []
+        for line in new_lines:
+            # Align the symbols in each cell
+            aligned_cells = [cell.strip().ljust(max_lengths[i]) for i, cell in enumerate(line)]
+
+            # Add spaces to the last element in every cell except empty cells
+            max_length = max(max_lengths)
+            aligned_cells = [cell[:-1] + ' ' + cell[-1] if cell and len(cell) < max_length else cell for cell in aligned_cells]
+
+            # Add a space after the first | character in each line
+            aligned_line = '| ' + ' | '.join(aligned_cells[1:])
+            aligned_lines.append(aligned_line)
+
+        # Generate the final result by enclosing it in the start and end markers of the Asciidoc table
+        return '|===\n' + '\n'.join(aligned_lines) + '\n|==='
+
+    # Apply the processing function to each table in the document
+    aligned_adoc_content = re.sub(table_pattern, process_table, adoc_content)
+    return aligned_adoc_content
+
+def main():
+    # Check if the number of command-line arguments is less than 2 (i.e., script name + at least one additional argument)
+    if len(sys.argv) < 2:
+        # If there are not enough arguments, print a usage message explaining how to use the script.
+        print("Usage: python script.py input.adoc")
+        # Return from the main function, terminating the script.
+        return
+
+    # Get the input filename from the command-line arguments.
+    input_file = sys.argv[1]
+
+    # Check if the input file exists in the current directory or the specified path.
+    if not os.path.exists(input_file):
+        # If the file does not exist, print an error message indicating that the file is not found.
+        print(f"File '{input_file}' does not exist!")
+        # Return from the main function, terminating the script.
+        return
+
+    # Open the input file and read its contents.
+    with open(input_file, 'r', encoding='utf-8') as file:
+        adoc_content = file.read()
+
+    # Call the processing function and get the aligned table contents
+    aligned_content = align_vertical_bars_in_tables(adoc_content)
+
+    # Replace 'output.adoc' with the desired path to the file where the aligned result will be saved.
+    output_file = 'output.adoc'
+    # Open the output file in write mode and save the aligned content into it.
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(aligned_content)
+    
+    # Print a success message to inform the user that table prettify is completed and where the result is saved.
+    print(f"Table prettify completed. Result saved in '{output_file}'.")
+
+# Check if this script is being run as the main program.
+if __name__ == "__main__":
+    # If the script is being run, call the main function to start the table prettify process.
+    main()
